f17e4caebd37db93dbc8f51021f3c03b29534518
Karen Herrera <karenjoha3838@gmail.com>
2024-03-14 10:38:49 -0500

    -

--- facturas/frontend/index.php
+++ facturas/frontend/index.php
@@ -178,7 +178,7 @@ if ($rol == 1 || $rol == 2 || $usuario == 'MANUELA MUÑOZ') { ?>
         </table>
 
 
-        <button type="button" class="btn btn-floating btn-lg" id="btn-back-to-top"><img src="assets/images/svg/arrow_up.svg" /></button>
+        <button type="button" class="btn btn-floating btn-lg" id="btn-back-to-top"><img src="../../assets/images/svg/arrow_up.png" /></button>
         </div>
         <div style="height:40px;"></div>
 
