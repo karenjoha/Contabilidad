52904ef637431a21b65ba221658e9666df03a165
karenjoha <karenjoha3838@gmail.com>
2024-03-17 20:38:16 -0500

    FALTA MUCHOOO

--- matriculas/backend/modelo/modelo.php
+++ matriculas/backend/modelo/modelo.php
@@ -2,106 +2,74 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/gestionadministrativa/conexion.php';
 
 class ModeloMatriculas {
-    public function mdlRegistro($tabla1, $tabla2, $tabla3, $datos) {
-        //INFO PRINCIPAL
 
-        $stmt = Conexion::conectar()->prepare(
-            "INSERT INTO $tabla1(fecha_registro, nombre_alum, primer_apellido, segundo_apellido, documento, tipo_documento) VALUES (:fecha_registro, :nombre_alum, :primer_apellido, :segundo_apellido, :documento, :tipo_documento)",
-        );
+public function mdlRegistro($tabla1, $tabla2, $tabla3, $datos) {
+    try {
+        // Inicia una transacción
+        Conexion::conectar()->beginTransaction();
 
-        $stmt->bindParam(
-            ":fecha_registro",
-            $datos["fecha_registro"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(":nombre_alum", $datos["nombre_alum"], PDO::PARAM_STR);
-        $stmt->bindParam(":primer_apellido", $datos["primer_apellido"], PDO::PARAM_STR);
-        $stmt->bindParam(
-            ":segundo_apellido",
-            $datos["segundo_apellido"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":documento",
-            $datos["documento"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":tipo_documento",
-            $datos["tipo_documento"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":fecha_nacimiento",
-            $datos["fecha_nacimiento"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":sexo",
-            $datos["sexo"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":lugar_nacimiento",
-            $datos["lugar_nacimiento"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":nacionalidad",
-            $datos["nacionalidad"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":barrio",
-            $datos["barrio"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(
-            ":direccion",
-            $datos["direccion"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(":estrato", $datos["estrato"], PDO::PARAM_STR);
-        $stmt->bindParam(
-            ":comuna",
-            $datos["comuna"],
-            PDO::PARAM_STR,
-        );
-        $stmt->bindParam(":celular", $datos["celular"], PDO::PARAM_STR);
-        $stmt->bindParam(":segundo_celular", $datos["segundo_celular"], PDO::PARAM_STR);
-        $stmt->bindParam(":fin_ctto", $datos["fin_ctto"], PDO::PARAM_STR);
-        $stmt->bindParam(":centro_cost", $datos["centro_cost"], PDO::PARAM_STR);
-        $stmt->bindParam(":barrio", $datos["barrio"], PDO::PARAM_STR);
-        $stmt->bindParam(":email", $datos["email"], PDO::PARAM_STR);
+        // Prepara la primera consulta para insertar datos del alumno
+        $stmt1 = Conexion::conectar()->prepare("INSERT INTO $tabla1 (fecha_registro, nombre_alum, primer_apellido, segundo_apellido, documento, tipo_documento, lugar_nacimiento, nacionalidad, direccion, barrio, estrato, comuna, celular, segundo_celular, email) VALUES (:fecha_registro, :nombre_alum, :primer_apellido, :segundo_apellido, :documento, :tipo_documento, :lugar_nacimiento, :nacionalidad, :direccion, :barrio, :estrato, :comuna, :celular, :segundo_celular, :email)");
+        // Asocia los parámetros de la primera consulta
+        $stmt1->bindParam(":fecha_registro", $datos["fecha_registro"], PDO::PARAM_STR);
+        $stmt1->bindParam(":nombre_alum", $datos["nombre_alum"], PDO::PARAM_STR);
+        $stmt1->bindParam(":primer_apellido", $datos["primer_apellido"], PDO::PARAM_STR);
+        $stmt1->bindParam(":segundo_apellido", $datos["segundo_apellido"], PDO::PARAM_STR);
+        $stmt1->bindParam(":documento", $datos["documento"], PDO::PARAM_STR);
+        $stmt1->bindParam(":tipo_documento", $datos["tipo_documento"], PDO::PARAM_STR);
+        $stmt1->bindParam(":lugar_nacimiento", $datos["lugar_nacimiento"], PDO::PARAM_STR);
+        $stmt1->bindParam(":nacionalidad", $datos["nacionalidad"], PDO::PARAM_STR);
+        $stmt1->bindParam(":direccion", $datos["direccion"], PDO::PARAM_STR);
+        $stmt1->bindParam(":barrio", $datos["barrio"], PDO::PARAM_STR);
+        $stmt1->bindParam(":estrato", $datos["estrato"], PDO::PARAM_STR);
+        $stmt1->bindParam(":comuna", $datos["comuna"], PDO::PARAM_STR);
+        $stmt1->bindParam(":celular", $datos["celular"], PDO::PARAM_STR);
+        $stmt1->bindParam(":segundo_celular", $datos["segundo_celular"], PDO::PARAM_STR);
+        $stmt1->bindParam(":email", $datos["email"], PDO::PARAM_STR);
 
-        //DATOS ACUDIENTE
-        $stmt2 = Conexion::conectar()->prepare("INSERT INTO $tabla2 (nombre_acudiente, celular_acudiente, parentesco, tipo_documento, documento) VALUES (:nombre_acudiente, :celular_acudiente, :parentesco, :tipo_documento, :documento)");
+        // Ejecuta la primera consulta
+        $stmt1->execute();
+
+        // Obtiene el último ID insertado en la primera tabla (id_alumno)
+        $id_alumno = Conexion::conectar()->lastInsertId();
+
+        // Prepara la segunda consulta para insertar datos del acudiente
+        $stmt2 = Conexion::conectar()->prepare("INSERT INTO $tabla2 (id_alumno, nombre_acudiente, celular_acudiente, parentesco, tipo_documento_acudiente, documento_acudiente) VALUES (:id_alumno, :nombre_acudiente, :celular_acudiente, :parentesco, :tipo_documento_acudiente, :documento_acudiente)");
+        // Asocia los parámetros de la segunda consulta
+        $stmt2->bindParam(':id_alumno', $id_alumno, PDO::PARAM_INT);
         $stmt2->bindParam(':nombre_acudiente', $datos['nombre_acudiente'], PDO::PARAM_STR);
         $stmt2->bindParam(':celular_acudiente', $datos['celular_acudiente'], PDO::PARAM_STR);
         $stmt2->bindParam(':parentesco', $datos['parentesco'], PDO::PARAM_STR);
-        $stmt2->bindParam(':tipo_documento', $datos['tipo_documento'], PDO::PARAM_STR);
-        $stmt2->bindParam(':documento', $datos['documento'], PDO::PARAM_STR);
+        $stmt2->bindParam(':tipo_documento_acudiente', $datos['tipo_documento_acudiente'], PDO::PARAM_STR);
+        $stmt2->bindParam(':documento_acudiente', $datos['documento_acudiente'], PDO::PARAM_STR);
+        // Ejecuta la segunda consulta
+        $stmt2->execute();
 
-
-        //INFO ADICIONAL
-        $stmt3 = Conexion::conectar()->prepare(
-            "INSERT INTO $tabla3(grupo, jornada, periodo_lectivo, procedencia) VALUES (:grupo, :jornada, :periodo_lectivo, :procedencia)",
-        );
+        // Prepara la tercera consulta para insertar datos del registro académico
+        $stmt3 = Conexion::conectar()->prepare("INSERT INTO $tabla3 (id_alumno, grupo, jornada, periodo_lectivo, procedencia) VALUES (:id_alumno, :grupo, :jornada, :periodo_lectivo, :procedencia)");
+        // Asocia los parámetros de la tercera consulta
+        $stmt3->bindParam(':id_alumno', $id_alumno, PDO::PARAM_INT);
         $stmt3->bindParam(':grupo', $datos['grupo'], PDO::PARAM_STR);
         $stmt3->bindParam(':jornada', $datos['jornada'], PDO::PARAM_STR);
         $stmt3->bindParam(':periodo_lectivo', $datos['periodo_lectivo'], PDO::PARAM_STR);
         $stmt3->bindParam(':procedencia', $datos['procedencia'], PDO::PARAM_STR);
+        // Ejecuta la tercera consulta
+        $stmt3->execute();
 
-        if ($stmt->execute() == true) {
-            $ultimo_id = Conexion::conectar()->lastInsertId();
-            $stmt2->execute() == true;
-            $stmt3->execute() == true;
+        // Confirma la transacción
+        Conexion::conectar()->commit();
 
-            return $ultimo_id;
-        } else {
-            print_r(Conexion::conectar()->errorInfo());
+        // Retorna el último ID insertado en la primera tabla
+        return $id_alumno;
+    } catch (PDOException $e) {
+        // Si ocurre un error, revierte la transacción
+        if (Conexion::conectar()->inTransaction()) {
+            Conexion::conectar()->rollBack();
         }
+        // Retorna el error
+        return $e->getMessage();
     }
+}
 
 
     private function verificarRegistroExistente($tabla, $idRecibimiento) {
@@ -122,7 +90,7 @@ class ModeloMatriculas {
         if ($item == null && $valor == null) {
             $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla1
 			INNER JOIN $tabla2 ON $tabla1.id_alumno = $tabla2.id_acudiente
-			INNER JOIN $tabla3 ON $tabla1.id_alumno = $tabla3.id_ficha_medica
+			INNER JOIN $tabla3 ON $tabla1.id_alumno = $tabla3.id_registro_academico
 			ORDER BY $tabla1.id_alumno DESC");
 
             $stmt->execute();
@@ -131,7 +99,7 @@ class ModeloMatriculas {
         } else {
             $stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla1
 			INNER JOIN $tabla2 ON $tabla1.id_alumno = $tabla2.id_acudiente
-			INNER JOIN $tabla3 ON $tabla1.id_alumno = $tabla3.id_ficha_medica
+			INNER JOIN $tabla3 ON $tabla1.id_alumno = $tabla3.id_registro_academico
 			WHERE $tabla1.$item = :$item"); // Aquí se califica la columna id_alumno
             $stmt->bindParam(":" . $item, $valor, PDO::PARAM_STR);
             $stmt->execute();
@@ -231,7 +199,7 @@ class ModeloMatriculas {
 
         //FICHA MEDICA
         $stmt3 = Conexion::conectar()->prepare(
-            "UPDATE $tabla3 SET grupo=:grupo, jornada=:jornada, periodo_lectivo=:periodo_lectivo, procedencia=:procedencia WHERE id_ficha_medica = :id_ficha_medica",
+            "UPDATE $tabla3 SET grupo=:grupo, jornada=:jornada, periodo_lectivo=:periodo_lectivo, procedencia=:procedencia WHERE id_registro_academico = :id_registro_academico",
         );
         $stmt3->bindParam(":grupo", $datos["grupo"], PDO::PARAM_STR);
         $stmt3->bindParam(":jornada", $datos["jornada"], PDO::PARAM_STR);
@@ -271,9 +239,9 @@ class ModeloMatriculas {
 
         //INFO ADICIONAL
         $stmt3 = Conexion::conectar()->prepare(
-            "DELETE FROM $tabla3 WHERE id_ficha_medica = :id_ficha_medica",
+            "DELETE FROM $tabla3 WHERE id_registro_academico = :id_registro_academico",
         );
-        $stmt3->bindParam(":id_ficha_medica", $valor3, PDO::PARAM_INT);
+        $stmt3->bindParam(":id_registro_academico", $valor3, PDO::PARAM_INT);
         // RESET AUTO_INCREMENT
         // $stmt4 = Conexion::conectar()->prepare("CALL resetear_ids()");
 
